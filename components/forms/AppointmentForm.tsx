42d3511b53f45a03d51e93f23dc34d1ec44f3ea0
keyurpatil06 <keyurpatil3000@gmail.com>
2024-07-17 15:07:53 +0530

    implement admin panel and sms feature

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -15,14 +15,17 @@ import { FormFieldType } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-  userId, patientId, type
+  userId, patientId, type, appointment, setOpen
 }: {
   userId: string;
   patientId: string,
   type: 'create' | 'cancel' | 'schedule';
+  appointment: Appointment;
+  setOpen: (open: boolean) => void;
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -32,11 +35,11 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: ""
+      primaryPhysician: appointment ? appointment.primaryPhysician : '',
+      schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+      reason: appointment ? appointment.reason : '',
+      note: appointment?.note || '',
+      cancellationReason: appointment?.cancellationReason || ''
     },
   })
 
@@ -49,7 +52,7 @@ const AppointmentForm = ({
         status = 'scheduled'
         break;
       case 'cancel':
-        status = 'canceled'
+        status = 'cancelled'
         break;
       default:
         status = 'pending'
@@ -68,10 +71,29 @@ const AppointmentForm = ({
           status: status as Status
         }
         const appointment = await createAppointment(appointmentData)
-        
+
         if (appointment) {
           form.reset();
           router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            primaryPhysician: values?.primaryPhysician,
+            schedule: new Date(values?.schedule),
+            status: status as Status,
+            cancellationReason: values?.cancellationReason,
+          },
+          type,
+        }
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
         }
       }
 
@@ -92,6 +114,7 @@ const AppointmentForm = ({
       buttonLabel = 'Create Appointment';
       break;
     case 'schedule':
+      console.log({ type });
       buttonLabel = 'Schedule Appointment';
       break;
 
@@ -102,10 +125,10 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
+        {type === 'create' && <section className="mb-12 space-y-4">
           <h1 className="header">New Appointment</h1>
           <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-        </section>
+        </section>}
 
         {type !== 'cancel' && (
           <>
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
-import { ID } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { ID, Query } from "node-appwrite";
+import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging } from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
   try {
@@ -28,6 +30,81 @@ export const getAppointment = async (appointmentId: string) => {
     )
 
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getRecentAppointmentList = async () => {
+  const appointments = await databases.listDocuments(
+    DATABASE_ID!,
+    APPOINTMENT_COLLECTION_ID!,
+    [Query.orderDesc("schedule")]
+  );
+
+  const initialCount = {
+    scheduledCount: 0,
+    pendingCount: 0,
+    cancelledCount: 0,
+  }
+
+  const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+    if (appointment.status === 'scheduled') {
+      acc.scheduledCount += 1;
+    } else if (appointment.status === 'pending') {
+      acc.pendingCount += 1;
+    } else if (appointment.status === 'cancelled') {
+      acc.cancelledCount += 1;
+    }
+
+    return acc;
+  }, initialCount);
+
+  const data = {
+    totalCount: appointments.total,
+    ...counts,
+    documents: appointments.documents
+  }
+
+  return parseStringify(data);
+}
+
+export const updateAppointment = async ({ appointmentId, userId, appointment, type }: UpdateAppointmentParams) => {
+  const updatedAppointment = await databases.updateDocument(
+    DATABASE_ID!,
+    APPOINTMENT_COLLECTION_ID!,
+    appointmentId,
+    appointment
+  );
+
+  if (!updatedAppointment) {
+    throw new Error('Appointment not found')
+  }
+
+  const smsMessage = `
+    Hi, it's CarePulse.
+    ${type === 'schedule'
+      ? `Your appointment is scheduled for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}`
+      : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`
+    }
+  `
+
+  await sendSMSNotification(userId, smsMessage);
+
+  revalidatePath('/admin');
+  return parseStringify(updatedAppointment);
+}
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId],
+    )
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
